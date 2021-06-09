f81a823175bbedbfe827de7aee8ca2e5bf833700
Camille Ocampo <cocampo0915@gmail.com>
2021-06-08 19:37:28 -0700

    wip: updates package.json

--- backend/server.js
+++ backend/server.js
@@ -33,4 +33,4 @@ app.use("/api/products", productRoute);
 //   res.send(data.products);
 // });
 
-app.listen(5000, () => { console.log("Server started at http://localhost:3001") });
+app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:3001") });
