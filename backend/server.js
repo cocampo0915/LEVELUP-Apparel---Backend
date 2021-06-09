b48baa6335e9a317fffdb6350f530f793e89874a
Camille Ocampo <cocampo0915@gmail.com>
2021-06-08 19:44:49 -0700

    config for deploy

--- backend/server.js
+++ backend/server.js
@@ -21,17 +21,4 @@ app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
-// app.get("/api/products/:id", (req, res) => {
-//   const productId = req.params.id;
-//   const product = data.products.find(x => x._id === productId);
-//   if (product)
-//     res.send(product);
-//   else
-//     res.status(404).send({ msg: "Product Not Found." })
-// });
-
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
-
-app.listen(5000, () => { console.log("Server started at http://localhost:5000") });
+app.listen(process.env.PORT || '5000', () => { console.log("Server started at http://localhost:5000") });
