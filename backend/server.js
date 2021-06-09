1f8944528230402575d58a55db37a243299e985a
Camille Ocampo <cocampo0915@gmail.com>
2021-06-08 20:35:14 -0700

    refactored package.json

--- backend/server.js
+++ backend/server.js
@@ -20,4 +20,4 @@ app.use(bodyParser.json());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 
-app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:3001") });
+app.listen(process.env.PORT || 5000, () => { console.log("Server started at http://localhost:5000") });
